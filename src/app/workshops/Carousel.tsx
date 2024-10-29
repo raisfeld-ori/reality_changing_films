4fb5a2905f16e8d6ff194242662bf2a037e3b12b
ori raisfeld <raisfeldori@gmail.com>
2024-10-29 16:16:43 +0200

    added images to caraousel

--- src/app/workshops/Carousel.tsx
+++ src/app/workshops/Carousel.tsx
@@ -4,10 +4,9 @@ import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 const images = [
-  "https://picsum.photos/id/237/1920/1080",
-  "https://picsum.photos/id/238/1920/1080",
-  "https://picsum.photos/id/239/1920/1080",
-  "https://picsum.photos/id/240/1920/1080",
+  "/image1.jpg",
+  "/image2.jpg",
+  "/image3.png",
 ]
 
 export default function Component() {
