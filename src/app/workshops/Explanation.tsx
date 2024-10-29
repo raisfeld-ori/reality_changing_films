5da7fd31790d2d22c7365442b343f563a6f65843
ori raisfeld <raisfeldori@gmail.com>
2024-10-29 16:20:29 +0200

    adde about us info

--- src/app/workshops/Explanation.tsx
+++ src/app/workshops/Explanation.tsx
@@ -11,7 +11,17 @@ export default function Component() {
         </h1>
         <div className="h-1 w-32 bg-blue-600 mx-auto mb-8"></div>
         <p className="text-xl md:text-2xl text-gray-700 leading-relaxed mb-12">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae libero ullam illum aperiam! Perferendis, necessitatibus repellat corrupti aperiam omnis, corporis eius, labore exercitationem totam qui odio itaque aliquid libero temporibus?
+        פעילות משמעותית ומרגשת לנוער יחד עם 
+במאי ושחקנים מקצועיים
+
+במהלך הסדנה, הנערים והנערות מקבלים הזדמנות לחוות ולשחק בסצנות המתמקדות בדילמות חברתיות ואישיות קשות שהם עלולים להתמודד איתן בחיים. 
+
+הסדנה מציעה למשתתפים חוויה של יצירתיות והבעת הערך עצמי דרך משחק.
+ובנוסף היא מעניקה כלים חשובים לאנשי החינוך להעביר מסרים עמוקים והשקפה על התנהגות חברתית והתמודדות עם קשיים. 
+כאשר הסצנות מוצגות בפני קהל וניתן פידבק חיובי בצורת מחיאות כפיים, נוצרת תחושת גאווה והערכה בקרב המשתתפים, מה שיכול להביא לחיזוק עצמי ולהנחותם לשפוט עצמם באופן חיובי.
+
+בסיום הסדנה בית הספר יקבל את הסרטונים
+של התלמידים ביום הקרנות מיוחד
         </p>
         <Link href={'/contact'}
           className="px-8 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
