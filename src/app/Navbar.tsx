98b86ceaedd610e6833d667f0b6bac67fcc0bbad
ori raisfeld <raisfeldori@gmail.com>
2024-10-29 15:27:02 +0200

    returned to original items

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -39,24 +39,24 @@ export default function Navbar() {
               >
                 לגבינו
               </Link>
-              <Link
-                href="/movies/first"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium h-20"
-              >
-                פנתר לבן
-              </Link>
-              <Link
-                href="/movies/second"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium h-20"
-              >
-                אל תחכי לי
-              </Link>
-              <Link
-                href="/movies/third"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium h-20"
-              >
-                אמצע החיים
-              </Link>
+              <div className="relative h-20 flex items-center">
+              <button
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium focus:outline-none h-full"
+                >
+                  סרטים
+                  <ChevronDown className="mr-1 h-4 w-4" />
+                </button>
+                {isDropdownOpen && (
+                  <div className="absolute right-0 top-full mt-1 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50 cursor-pointer">
+                    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                      <a href="/movies/first" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">פנתר לבן</a>
+                      <a href="/movies/second" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">אל תחכי לי</a>
+                      <a href="/movies/third" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">אמצע החיים</a>
+                    </div>
+                  </div>
+                )}
+              </div>
               <Link
                 href="/workshops"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium h-20"
