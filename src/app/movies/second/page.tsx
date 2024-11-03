ac969fa535172d34d335d3b447414dc8a57f72fa
ori raisfeld <raisfeldori@gmail.com>
2024-11-03 17:17:29 +0200

    fixed movie content

--- src/app/movies/second/page.tsx
+++ src/app/movies/second/page.tsx
@@ -1,5 +1,12 @@
 import Movie from '../movie';
 export default function Page(){
-    return <Movie title='אל תחכי לי' description={[""]} trailerSrc='/first.mp4' 
-        backgroundImageSrc='https://picsum.photos/id/261/5000/5000'></Movie>
+    return <Movie title='אל תחכי לי' description={[
+        "סרט חברתי מסחרי לנוער, בכיכובים של הנוער עומר חזן, טיילו מלכוב ואורי גבריאל ועינת שרוף",
+        "סרט המספר סיפור גאולה על נער שמנסה לשרוד בשכונת מצוקה עליה הגיע אחרי שמפחתו פשטה את הרגל",
+        "מבוסס על סיפור אמיתי.",
+        "נושאים: מאבק באלימות, זהות ישראלית, זכויות אדם, יום רבין, הכלה והשלבות קבלת החלטות, מעברים (חטיבה לתיכון), חוסן והתמודדות",
+        "גרסאות וסדנאות מותאמות שכבת גיל ט-יב",
+        "הומלץ על ידי שרת החינוך ד\"ר יפעת שאשא ביטון"
+    ]} trailerSrc='/dontwaitformevideo.mp4' 
+        backgroundImageSrc='/dontwaitforme.jpg'></Movie>
 }
