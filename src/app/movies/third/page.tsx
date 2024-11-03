e348072166d717f5608f7e52ec7ac95adb502de9
ori raisfeld <raisfeldori@gmail.com>
2024-11-03 17:26:05 +0200

    updated content

--- src/app/movies/third/page.tsx
+++ src/app/movies/third/page.tsx
@@ -1,7 +1,11 @@
 import Movie from '../movie';
 export default function Page(){
     return <Movie title='אמצע החיים' description={[
-        
-    ]} trailerSrc='/first.mp4' 
-        backgroundImageSrc='https://picsum.photos/id/416/5000/5000'></Movie>
+        'סרט חברתי בכיכובם של צחי הלוי, שלום אסייג, חן אמסלם, יניב סויסה (קופה ראשית).',
+        'סיפור על ישראליות, אחדות, אהבה, תשוקה, ואמונה הסוחפת אותך באמצע החיים.',
+        "נושאים: זהות ישראלית, מיניות בריאה, קבחת החלטות, אמונה וחוסן",
+        "גרסאות וסדנאות מותאמות שכבת גיל י-יב",
+
+    ]} trailerSrc='/middlelifetrailer.mp4' 
+        backgroundImageSrc='/middlelife.jpg'></Movie>
 }
