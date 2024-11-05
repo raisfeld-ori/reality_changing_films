cbfb0260d16dcce83643c45b76c1fec705e4456a
ori raisfeld <raisfeldori@gmail.com>
2024-11-05 11:01:50 +0200

    removed unused tool useeffect

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Star } from "lucide-react"
 import Image from 'next/image'
