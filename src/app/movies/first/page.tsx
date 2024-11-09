ad81819e1bf1c38dfde49ddfca3c6d7640fa5572
ori raisfeld <raisfeldori@gmail.com>
2024-11-09 15:30:14 +0200

    updated content

--- src/app/movies/first/page.tsx
+++ src/app/movies/first/page.tsx
@@ -6,11 +6,12 @@ export default function Page(){
     ' סרט המספר את המסע לאליפות של אלקס, נער זועם וכועס ושל המאמן שלו דוד אוחנה (זאב רווח האגדי)',
     'הנושאים והימים להם הוא מתאים: גזענות, עלייה, זכויות אדם, יום רבין, זהות ישראלית, הכלה והשתלבות, מאבק באלימות.',
     'המלצת מפמ"רית תחום קולנוע משרד החינוך לשכבות ז-יב'
-    ]} trailerSrc='/whitepanthertrailer.mp4' content={[{
-        title: 'סרט פנתר לבן',
-        media: {type: 'image', src:'/whitepanther1.jpg', text: ''},
-        text: 'סרט פנתר לבן לחצו לכפתור הסרט לפרס סרטי הטוב ביותר באוסקר הישראלי.'
-        
-    }]}
+    ]} trailerSrc='/whitepanthertrailer.mp4' content={[
+        {
+            title: '',
+            text: '',
+            media: {type: 'link', src:'/whitepanther1.jpg', text: ''},
+        }
+    ]}
         backgroundImageSrc='/whitepanther.jpg'></Movie>
 }
